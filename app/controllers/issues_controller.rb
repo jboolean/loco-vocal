12d06f20120e1c7dcb0ba1bc4bacf43661fc1c77
Julian Boilen <julianboilen@gmail.com>
2014-10-28 15:22:48 -0400

    editing and adding issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -3,10 +3,28 @@ class IssuesController < ApplicationController
 
   def show
     @issue = Issue.find(params[:id])
+    @title = @issue.title
     @mdparser = @@mdparser
   end
+
   def new
+    @issue = Issue.new
   end
+
+  def edit
+    @issue = Issue.find(params[:id])
+  end
+
+  def update
+    @issue = Issue.find(params[:id])
+   
+    if @issue.update(issue_params)
+      redirect_to @issue
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @issue = Issue.create(issue_params)
     redirect_to @issue
